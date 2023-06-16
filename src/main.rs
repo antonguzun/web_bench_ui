001d4b53dbc375542768171b24d5322521526542
antonguzun <aguzun@ibitcy.com>
2023-06-16 15:00:43 +0300

    s

--- src/main.rs
+++ src/main.rs
@@ -77,44 +77,34 @@ fn video_details(VideosDetailsProps { video }: &VideosDetailsProps) -> Html {
 
 #[function_component]
 fn App() -> Html {
-    let videos = use_state(|| vec![]);
+    let videos = use_state(|| None);
     {
         let videos = videos.clone();
         use_effect_with_deps(
             move |_| {
                 let videos = videos.clone();
                 wasm_bindgen_futures::spawn_local(async move {
-                    let fetched_videos: Vec<Video> = Request::get("/tutorial/data.json")
+                    let fetched_videos: Root = Request::get("/web_bench/temp")
                         .send()
                         .await
                         .unwrap()
                         .json()
                         .await
                         .unwrap();
-                    videos.set(fetched_videos);
+                    videos.set(Some(fetched_videos));
                 });
                 || ()
             },
             (),
         );
     }
-    let selected_video = use_state(|| None);
-    let on_video_select = {
-        let selected_video = selected_video.clone();
-        Callback::from(move |video: Video| selected_video.set(Some(video)))
-    };
-    let details = selected_video.as_ref().map(|video| {
-        html! {
-            <VideoDetails video={video.clone()} />
-        }
-    });
     html! {
         <>
             <h1>{ "RustConf Explorer" }</h1>
             <div>
                 <h3>{"Videos to watch"}</h3>
-                <VideosList videos={(*videos).clone()} on_click={on_video_select.clone()} />
-                { for details }
+                // <VideosList videos={(*videos).clone()} on_click={on_video_select.clone()} />
+                // { for details }
             </div>
         </>
     }
